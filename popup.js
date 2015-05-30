8be4cd03ddebc99aa7c789fdc62674eb99f02b77
Pete Milkman <pmilkman@gmail.com>
2015-05-30 14:57:55 -0400

    click URL head to see full URL's

--- popup.js
+++ popup.js
@@ -12,8 +12,8 @@
 
         function updateList() {
             var bgeo = chrome.extension.getBackgroundPage().eventObject;
-            console.log(chrome.extension.getBackgroundPage());
             var newhtml = '';
+            var dispURL = '';
             if(bgeo.length) {
                 var countUntil;
                 if(bgeo.length > 15) {
@@ -23,15 +23,18 @@
                 }
                 for(var i = bgeo.length-1; i > countUntil; i--) {
                     if(bgeo[i][0].length > 37) {
-                        bgeo[i][0] = bgeo[i][0].substr(0, 32) + ' ...';
+                        dispURL = bgeo[i][0].substr(0, 32) + ' ...';
                     }
-                    newhtml += '<tr>';
-                    newhtml += '<td>' + bgeo[i][0] + '</td>';
-                    newhtml += '<td>' + bgeo[i][1] + '</td>';
-                    newhtml += '<td>' + bgeo[i][2] + '</td>';
-                    newhtml += '<td>' + bgeo[i][3] + '</td>';
-                    newhtml += '<td>' + bgeo[i][4] + '</td>';
-                    newhtml += '<td>' + bgeo[i][5] + '</td>';
+                    newhtml += '<tr class="entry">';
+                        newhtml += '<td class="url">';
+                            newhtml += '<span class="compact">' + dispURL + '</span>';
+                            newhtml += '<span class="full">' + bgeo[i][0] + '</span>';
+                        newhtml += '</td>';
+                        newhtml += '<td class="uastring">' + bgeo[i][1] + '</td>';
+                        newhtml += '<td>' + bgeo[i][2] + '</td>';
+                        newhtml += '<td>' + bgeo[i][3] + '</td>';
+                        newhtml += '<td>' + bgeo[i][4] + '</td>';
+                        newhtml += '<td>' + bgeo[i][5] + '</td>';
                     newhtml += '</tr>';
                 }
                 newhtml += '<tr><td colspan="6" align="center"><a href="javascript:void(0)" id="reset-list">Reset List</a></td></tr>';
@@ -50,6 +53,15 @@
         // Call the getPageInfo function in the background page, passing in 
         // our onPageInfo function as the callback
         window.onload = function() { 
+            document.getElementById('expander').onclick = function() {
+                var table = document.getElementById('event-table');
+                var tableClasses = table.classList;
+                if(tableClasses.contains('wrapit')) {
+                    tableClasses.remove('wrapit');
+                } else {
+                    tableClasses.add('wrapit');
+                }
+            };
             updateList();
             setInterval(updateList, 3000);
         };
